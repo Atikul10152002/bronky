975d05ca3d972656fda22af32d865e6504ef2a59
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2018-08-07 12:44:01 -0400

    Update testmain.py

--- main.py
+++ main.py
@@ -2,11 +2,8 @@
 
 # imports the necessary libraries
 import sys
-# THE NEXT LINE IS ONLY FOR DOCUMENTATIONS
-import fakesys as sys
 import timer
 import vex
-# import sys
 
 # MOTORS
 _claw = vex.Motor(2)
@@ -19,13 +16,13 @@ _backLeft = vex.Motor(7, True)
 
 # JOYSTICK
 joystick = vex.Joystick()
-joystick.set_deadband(15)
+joystick.set_deadband(10)
 
 
 # VARIBALES
 # global variables
-
-AUTO_TIME = 5
+MAXIMUM_MOTOR_SPEED = 100
+maxAUTOTIME = 1
 WRIST_UP_POWER = 50
 WRIST_DOWN_POWER = -(50)
 CLAW_CLOSE_POWER = 50
@@ -34,13 +31,7 @@ CLAW_CONSTANT_CLOSE_POWER = 10
 
 
 def CLAW_DRIVE():
-    """
-    Controls the claw motor
-
-    The fuctions acts as a togggle
-
-    When the claw is closed it applies continious power to the motor
-    """
+    # timer variables
     tickTimer = timer.Timer()
     tickTimer_max = 1.3    # Seconds
     tickTimer.start()
@@ -48,33 +39,30 @@ def CLAW_DRIVE():
     buttonTimer_max = 1.3  # Seconds
     buttonTimer.start()
     clawIsOpen = False
-
+    
     while True:
         # print 'TELEOP CLAW'
         # CLAW MOTORS
         if joystick.b8down() and buttonTimer.elapsed_time() > buttonTimer_max:
-            if not clawIsOpen:
-                clawIsOpen = True
-                # RESET TICK TIMER
-                tickTimer.start_lap()
-
-            elif clawIsOpen:
+            if clawIsOpen:
                 clawIsOpen = False
                 # RESET TICK TIMER
                 tickTimer.start_lap()
-
+            elif clawIsOpen is False:
+                clawIsOpen = True
+                # RESET TICK TIMER
+                tickTimer.start_lap()
             # RESET BUTTON TIMER
             buttonTimer.start_lap()
-
+        
         if tickTimer.elapsed_time() < tickTimer_max:
-            if not clawIsOpen:
-                # IF CLAW IS CLOSED OPEN IT
-                _claw.run(CLAW_OPEN_POWER)
-
-            elif clawIsOpen:
+            if clawIsOpen:
                 # IF CLAW IS OPEN CLOSE IT
                 _claw.run(CLAW_CLOSE_POWER)
-
+            elif clawIsOpen is False:
+                # IF CLAW IS CLOSED OPEN IT
+                _claw.run(CLAW_OPEN_POWER)
+    
         # CONTINIOUS POWER TO CLOSE THE CLAW
         else:
             if clawIsOpen:
@@ -86,21 +74,16 @@ def CLAW_DRIVE():
 
 
 def BASE_DRIVE():
-    """
-| Controls the base motors -- Arcade drive
-| Calculates the power for the left and right drive 
-and runs the four base motors
-    """
     while True:
         # print 'TELEOP BASE'
         # BASE MOTORS
         # ARCADE DRIVE
-        forward = joystick.axis3()
-        steer = joystick.axis4()
-
+        forward = joystick.axis3() 
+        steer = joystick.axis4() 
+    
         # calculate left and right power
-        left = forward + steer
-        right = forward - steer
+        left = min((forward + steer), MAXIMUM_MOTOR_SPEED)
+        right = min((forward - steer), MAXIMUM_MOTOR_SPEED)
 
         # LEFT DRIVE
         _forwardLeft.run(left)
@@ -111,10 +94,6 @@ and runs the four base motors
 
 
 def WRIST_DRIVE():
-    """
-| Controls the wrist motors
-| Only when the buttons are pressed, the Motors receive power.
-    """
     while True:
         # print 'TELEOP WRIST'
         # WRIST MOTORS
@@ -125,39 +104,28 @@ def WRIST_DRIVE():
         else:
             _wrist.off()
 
+# autoTimer = timer.Timer()
+# autoTimer.start()
 
 def autonomous():
-    """
-| This is the autonomous period before the Teleop statsself.
-| Still in test phase --
-    """
-    import random
-    lef_choice = random.choice([-50, 50])
-    rig_choice = lef_choice * -1
-    print(rig_choice)
     while True:
-        _backLeft.run(lef_choice)
-        _forwardLeft.run(lef_choice)
-        _backRight.run(rig_choice)
-        _forwardRight.run(rig_choice)
-        # print "AUTO", sys.clock()
-        if AUTO_TIME < sys.clock():
-            print('AUTO FINISHED')
+        _backLeft.run(50)
+        _forwardLeft.run(50)
+        _backRight.run(-50)
+        _forwardRight.run(-50)
+        print "AUTO", sys.clock()
+        
+        if maxAUTOTIME < sys.clock():
             break
 
 
 def driver():
-    """
-| This is the Teleop mode where the driver has the control over the robot.
-| The fucntion runs the claw, base and wrist in different threads
-    """
-    print("TELEOP running")
+    print ("TELEOP running")
     sys.run_in_thread(CLAW_DRIVE)
     sys.run_in_thread(BASE_DRIVE)
     sys.run_in_thread(WRIST_DRIVE)
 
 
-
-# THE FOLLOWING LINE IS COMMENTED OUT FOR DOCUMENTATION
-# autonomous()
+autonomous()
+print("autoFINISHED")    
 driver()
